4ca50c38a886269ed3f389aa4fa9581a9cf7633b
Sergey <Sekgir@mail.ru>
2021-07-16 10:27:52 +0300

    Исправлен формат даты при запросе метрик с агентов (добалены указания по часовому поясу). Реализованы методы GET для запроса метрик агентов у менеджера.

--- MetricsManager/Controllers/CpuMetricsController.cs
+++ MetricsManager/Controllers/CpuMetricsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using MetricsManager.DAL.Interfaces;
+using MetricsManager.DAL.Models;
+using MetricsManager.DTO;
+using MetricsManager.Responses;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,26 +16,40 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
+        private readonly ICpuMetricsRepository _repository;
         private readonly ILogger<CpuMetricsController> _logger;
+        private readonly IMapper _mapper;
 
-        public CpuMetricsController(ILogger<CpuMetricsController> logger)
+        public CpuMetricsController(ILogger<CpuMetricsController> logger, ICpuMetricsRepository repository, IMapper mapper)
         {
             _logger = logger;
             _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+            _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
         {
             _logger.LogInformation($"Выполнение метода GetMetricsFromAgent(agentId = {agentId}, fromTime = {fromTime}, toTime = {toTime})");
-            return Ok();
+            var metrics = _repository.GetByTimePeriodFromAgent(agentId, fromTime, toTime);
+            var response = new AllCpuMetricsApiResponse()
+            {
+                Metrics = _mapper.Map<IList<CpuMetric>, List<CpuMetricDto>>(metrics)
+            };
+            return Ok(response);
         }
 
         [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAllCluster([FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
         {
             _logger.LogInformation($"Выполнение метода GetMetricsFromAllCluster(fromTime = {fromTime}, toTime = {toTime})");
-            return Ok();
+            var metrics = _repository.GetByTimePeriodFromAllCluster(fromTime, toTime);
+            var response = new AllCpuMetricsApiResponse()
+            {
+                Metrics = _mapper.Map<IList<CpuMetric>, List<CpuMetricDto>>(metrics)
+            };
+            return Ok(response);
         }
     }
 }
--- MetricsManager/Controllers/NetworkMetricsController.cs
+++ MetricsManager/Controllers/NetworkMetricsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using MetricsManager.DAL.Interfaces;
+using MetricsManager.DAL.Models;
+using MetricsManager.DTO;
+using MetricsManager.Responses;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,26 +17,40 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class NetworkMetricsController : ControllerBase
     {
+        private readonly INetworkMetricsRepository _repository;
         private readonly ILogger<NetworkMetricsController> _logger;
+        private readonly IMapper _mapper;
 
-        public NetworkMetricsController(ILogger<NetworkMetricsController> logger)
+        public NetworkMetricsController(ILogger<NetworkMetricsController> logger, INetworkMetricsRepository repository, IMapper mapper)
         {
             _logger = logger;
             _logger.LogDebug(1, "NLog встроен в NetworkMetricsController");
+            _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
         {
             _logger.LogInformation($"Выполнение метода GetMetricsFromAgent(agentId = {agentId}, fromTime = {fromTime}, toTime = {toTime})");
-            return Ok();
+            var metrics = _repository.GetByTimePeriodFromAgent(agentId, fromTime, toTime);
+            var response = new AllNetworkMetricsApiResponse()
+            {
+                Metrics = _mapper.Map<IList<NetworkMetric>, List<NetworkMetricDto>>(metrics)
+            };
+            return Ok(response);
         }
 
         [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAllCluster([FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
         {
             _logger.LogInformation($"Выполнение метода GetMetricsFromAllCluster(fromTime = {fromTime}, toTime = {toTime})");
-            return Ok();
+            var metrics = _repository.GetByTimePeriodFromAllCluster(fromTime, toTime);
+            var response = new AllNetworkMetricsApiResponse()
+            {
+                Metrics = _mapper.Map<IList<NetworkMetric>, List<NetworkMetricDto>>(metrics)
+            };
+            return Ok(response);
         }
     }
 }
